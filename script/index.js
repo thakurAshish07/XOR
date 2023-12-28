ee5f2534f5dcbdcdf6d70e3ee796d7cced9869af
Ashish Tomar <49639269+thakurAshish07@users.noreply.github.com>
2023-12-28 20:09:16 +0530

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,21 +1,4 @@
 let bagItems = [];
-displayItemsOnHomePage();
-bagItemCount();
-
-function addToBag(itemId) {
-    bagItems.push(itemId);
-    bagItemCount();
-}
-
-function bagItemCount(){
-    let bagItemCountElement = document.querySelector('.bag-item-count');
-    if (bagItems.length > 0){
-        bagItemCountElement.innerHTML = bagItems.length;
-        bagItemCountElement.style.visibility = 'visible';
-    } else {
-        bagItemCountElement.style.visibility = 'hidden';
-    }
-}let bagItems = [];
 onLoad();
 
 function onLoad(){
@@ -64,22 +47,3 @@ gsap.from(".items-container", {
     duration:1.5,
     opacity:0,
 })
-
-
-function displayItemsOnHomePage() {
-    let itemContainerElement = document.querySelector('.items-container');
-
-    let innerHtml = '';
-    items.forEach(item => {
-        innerHtml += `
-        <div class="item-container">
-            <img class="item-image" src="${item.item_image}" alt="">
-            <div class="company-name">${item.company_name}</div>
-            <div class="item-name">${item.item_name}</div>
-            <div class="price">${item.price}</div>
-            <button class="addToCart" onclick="addToBag(${item.id})">Add to cart</button>
-        </div>
-        `
-    });
-    itemContainerElement.innerHTML = innerHtml;
-}
